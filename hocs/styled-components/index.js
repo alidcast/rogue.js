4533847e0409feea6f9937631e1821b65915d773
Alid Castano <alidcastano@gmail.com>
2018-07-04 10:35:42 -0500

    cleaned up style hocs

--- hocs/styled-components/index.js
+++ hocs/styled-components/index.js
@@ -1,9 +1,14 @@
-const { isServer } = require('../../index')
-
 const { createElement: h } = require('react')
 const { ThemeProvider } = require('styled-components')
 
-
+/*
+* Styled Components Hoc to configure styles for server side rendering.
+*
+* Should be used as follows: 
+*     withStyles(theme)(App)
+* 
+* Initializing the hoc is optional. But our hoc must be imported so that we know to configure SRR support.
+*/
 const withStyles = (theme = {}) => App => {
   function RogueStyleProvider (props) {
     return h(ThemeProvider, { theme }, h(App, props))
